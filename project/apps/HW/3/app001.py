f02cfbe209893532300e7a2b2625bd62438cd9d7
bonew <bonew@bonew.ru>
2023-12-24 23:24:29 +0300

    add hw3

--- project/apps/HW/3/app001.py
+++ project/apps/HW/3/app001.py
@@ -4,8 +4,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SECRET_KEY'] = 'your_secret_key'  # Замените на свой секретный ключ
+app.config['SECRET_KEY'] = '!st1!#=y(yo+!r@l+oami+1ae$+5a5+gc4wj!-!&8ahi)^)%$z'
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,12 +15,15 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
+
 with app.app_context():
     db.create_all()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -29,16 +33,18 @@ def register():
         email = request.form['email']
         password = request.form['password']
 
-        # Хеширование пароля перед сохранением в базу данных
-        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        hashed_password = generate_password_hash(
+            password, method='pbkdf2:sha256')
 
-        new_user = User(first_name=first_name, last_name=last_name, email=email, password=hashed_password)
+        new_user = User(first_name=first_name, last_name=last_name,
+                        email=email, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
 
         flash('Регистрация прошла успешно!', 'success')
 
         return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
